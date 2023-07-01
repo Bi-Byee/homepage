2dfe3357ba4658c40e7224723d4b6686d162b801
Karthik <132189261+Bi-Byee@users.noreply.github.com>
2023-07-01 19:37:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,13 @@ var exit = document.querySelector('.menu img')
 var menu =document.querySelector('.menu')
 
 
-window.addEventListener('resize', function() {
     var windowWidth = window.innerWidth;
-    
     if (windowWidth > 426) {
       mech.src='assets/images/image-web-3-desktop.jpg';
     } else {
         mech.src='assets/images/image-web-3-mobile.jpg';
     }
-  } 
-);
+
 
 menub.addEventListener('click', function() {
    menu.style.display = 'flex'; 
